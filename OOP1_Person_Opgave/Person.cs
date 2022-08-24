89e9e0a7c20e2173af37a4759e13d9aa99a781c6
johan <johan@DESKTOP-Q9870N1>
2022-08-24 12:48:19 +0200

    Add project files.

---
+++ OOP1_Person_Opgave/Person.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1PersonOpgave
+{
+    internal class Person
+    {
+        DateTime today = DateTime.Today;
+        private string name;
+        private DateTime dob;
+        public bool checkTrue = true;
+        public bool doesStringContain = true;       
+        private string email;
+        private string password;
+
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (value == "" || value.All(char.IsDigit)) doesStringContain = false;
+                else doesStringContain = true;
+            }
+        }
+
+        public string DoB
+        {
+
+            get { return dob.ToString(); }
+            set
+            {
+                if (DateTime.TryParse(value, out dob)) checkTrue = false;
+                else checkTrue = true;
+
+            }
+        }
+
+        public int Age
+        {
+            get { return today.Year - dob.Year; }
+
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set
+            {
+                if (value.Contains("@") && (value.Contains("."))) doesStringContain = true;
+                else doesStringContain = false;
+            }
+        }
+
+        public string Password
+        {
+            get { return password; }
+            set
+            {
+                if (value.Length > 7 && value.Any(char.IsUpper) && value.Any(char.IsLower) && value.Any(char.IsDigit) && !value.Contains(" ")) doesStringContain = true;
+                else doesStringContain = false;
+            }
+        }
+
+    }
+}
---
+++ OOP1_Person_Opgave/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace OOP1PersonOpgave;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Person person = new Person();
+
+
+        do
+        {
+            Console.WriteLine("Enter your name:");
+            person.Name = Console.ReadLine();
+            Console.WriteLine(person.Name);
+
+        } while (person.doesStringContain == false);
+
+        do
+        {
+            Console.WriteLine("Enter your date of birth: ");
+            person.DoB = Console.ReadLine();            
+            Console.WriteLine(person.DoB);
+            Console.WriteLine($"Din alder er {person.Age}");
+
+        } while (person.checkTrue == true);
+
+        do
+        {
+            Console.WriteLine("Enter your email:");
+            person.Email = Console.ReadLine();
+            Console.WriteLine(person.Email);
+
+        } while (person.doesStringContain == false);
+
+        do
+        {
+            Console.WriteLine("Enter your password:");
+            person.Password = Console.ReadLine();
+            Console.WriteLine(person.Password);
+
+        } while (person.doesStringContain == false);
+
+
+    }
+
+}
